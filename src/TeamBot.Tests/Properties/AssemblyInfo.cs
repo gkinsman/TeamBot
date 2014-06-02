0a05d46c7f2fdaadde39478afa69a75cd24382c6
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-06-02 22:48:52 +1000

    Moved the version number into separate AssemblyInfo

--- src/TeamBot.Tests/Properties/AssemblyInfo.cs
+++ src/TeamBot.Tests/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("TeamBot.Tests")]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
--- src/TeamBot/Properties/AssemblyInfo.cs
+++ src/TeamBot/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("TeamBot")]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
