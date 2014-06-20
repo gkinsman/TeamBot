669c9c5674e9aff816a2af54466d140c34e2e8a8
Gert Jansen van Rensburg <gertjvr@gmail.com>
2014-06-20 22:50:15 +1000

    Added weather feature.

--- src/TeamBot/Features/Giphy/GiphyHandler.cs
+++ src/TeamBot/Features/Giphy/GiphyHandler.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using TeamBot.Infrastructure.Messages;
 using TeamBot.Infrastructure.Slack;
 using TeamBot.Infrastructure.Slack.Models;
@@ -11,15 +15,13 @@ namespace TeamBot.Features.Giphy
 {
     public class GiphyHandler : SlackMessageHandler
     {
-        private readonly IGiphyClient _giphy;
+        public const string GiphyApiKey = "GiphyApiKey";
+        private readonly string _giphyUri;
 
-        public GiphyHandler(ISlackClient slack, IGiphyClient giphy)
+        public GiphyHandler(ISlackClient slack)
             : base(slack)
-        {
-            if (giphy == null) 
-                throw new ArgumentNullException("giphy");
-
-            _giphy = giphy;
+        {   
+            _giphyUri = "http://api.giphy.com/v1/gifs";
         }
 
         public override string Help()
@@ -35,6 +37,7 @@ namespace TeamBot.Features.Giphy
             var patterns = new Dictionary<string, Func<IncomingMessage, Match, Task>>
 		    {
                 { "(giphy|gif)( me)? (.*)", async (message, match) => await GiphyAsync(message, match.Groups[3].Value) },
+                { "giphy apikey (.*)", async (message, match) => await LoadApiKeyAsync(message, match.Groups[3].Value) },
 		    };
 
             foreach (var pattern in patterns)
@@ -45,6 +48,13 @@ namespace TeamBot.Features.Giphy
             }
         }
 
+        private async Task LoadApiKeyAsync(IncomingMessage incomingMessage, string input)
+        {
+            Brain[GiphyApiKey] = input;
+
+            await Slack.SendAsync(incomingMessage.ReplyTo(), string.Format("@{0} Giphy ApiKey Set", incomingMessage.UserName));
+        }
+
         private async Task GiphyAsync(IncomingMessage incomingMessage, string input)
         {
             Exception exception = null;
@@ -52,13 +62,13 @@ namespace TeamBot.Features.Giphy
             {
                 if (string.IsNullOrEmpty(input) || input.ToLower().StartsWith("random"))
                 {
-                    await Slack.SendAsync(incomingMessage.ReplyTo(), string.Format("@{0} {1}", incomingMessage.UserName, await _giphy.Random()));
+                    await Slack.SendAsync(incomingMessage.ReplyTo(), string.Format("@{0} {1}", incomingMessage.UserName, await Random()));
                     return;
                 }
 
                 var random = new Random();
                 var offset = random.Next(3);
-                var results = await _giphy.Search(incomingMessage.Text, 10, offset);
+                var results = await Search(incomingMessage.Text, 10, offset);
                 var max = results.Count();
 
                 var image = max <= 0
@@ -77,6 +87,54 @@ namespace TeamBot.Features.Giphy
             {
                 var response = string.Format("@{0} Umm... something went wrong  \"{1}\" {2}", incomingMessage.UserName, incomingMessage.Text, exception.Message);
                 await Slack.SendAsync(incomingMessage.ReplyTo(), response);
+            }
+        }
+
+        private async Task<string[]> Search(string input, int limit = 10, int offset = 0)
+        {
+            using (var client = new HttpClient())
+            {
+                var giphyApiKey = (string)Brain[GiphyApiKey];
+                var uri = string.Format("{0}/search?api_key={1}&q={2}&limit={3}&offset={4}", _giphyUri, giphyApiKey, HttpUtility.UrlEncode(input), limit, offset);
+
+                var response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                var results = JsonConvert.DeserializeObject<dynamic>(content);
+
+                int max = results.data.Count ?? 0;
+
+                if (results.meta.status != 200 || max <= 0)
+                    return Enumerable.Empty<string>().ToArray();
+
+                var urls = new List<string>();
+                foreach (var result in results.data)
+                    urls.Add(string.Format("{0}", result.images.fixed_height.url));
+
+                return urls.ToArray();
+            }
+        }
+
+        private async Task<string> Random(string input = null)
+        {
+            using (var client = new HttpClient())
+            {
+                var giphyApiKey = (string)Brain[GiphyApiKey];
+                var uri = input != null
+                    ? string.Format("{0}/random?api_key={1}&tag={2}", _giphyUri, giphyApiKey, HttpUtility.UrlEncode(input))
+                    : string.Format("{0}/random?api_key={1}", _giphyUri, giphyApiKey);
+
+                var response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<dynamic>(content);
+
+                if (result.meta.status != 200 || (result.data as JArray) != null)
+                    return null;
+
+                return (string)result.data.image_url;
             }
         }
     }
