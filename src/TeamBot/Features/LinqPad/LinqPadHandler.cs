96795143a968ca8b5d584be89e85aaa42cc67c9c
George Kinsman <george@georgekinsman.com>
2016-07-17 17:44:29 +1000

    Remove bad reference

--- src/TeamBot/Features/LinqPad/LinqPadHandler.cs
+++ src/TeamBot/Features/LinqPad/LinqPadHandler.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using JetBrains.dotMemoryUnit.Util;
 using Nancy;
 using TeamBot.Infrastructure.Messages;
 using TeamBot.Infrastructure.Slack;
