abbfddcac1f87c7f6ad083cc5a003068d2a80416
George Kinsman <george@georgekinsman.com>
2016-07-20 00:07:49 +1000

    Let's see if lets us change the script type!

--- src/TeamBot/Features/LinqPad/LinqPadHandler.cs
+++ src/TeamBot/Features/LinqPad/LinqPadHandler.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Humanizer;
 using Nancy;
 using Serilog;
+using Sprache;
 using TeamBot.Infrastructure.Messages;
 using TeamBot.Infrastructure.Slack;
 using TeamBot.Infrastructure.Slack.Models;
@@ -44,12 +45,58 @@ namespace TeamBot.Features.LinqPad
             return null;
         }
 
+        private class KeyValue
+        {
+            public string Key { get; set; }
+            public string Value { get; set; }
+        }
+
+        private class ScriptToRun
+        {
+            public IList<KeyValue> Args { get; set; }
+            public string Script { get; set; }
+
+            public string ToArgsString()
+            {
+                return Args.Aggregate("", (argString, arg) => argString + $"{arg.Key}={arg.Value} ").Trim();
+            }
+        }
+
+        private ScriptToRun ParseInput(string both)
+        {
+            var trimmed = both.Trim();
+
+            var flagParser =
+                from leading in Parse.Char('-')
+                from flag in Parse.Letter.AtLeastOnce().Text()
+                from separator in Parse.Char('=')
+                from value in Parse.LetterOrDigit.AtLeastOnce().Text()
+                from whiteSpace in Parse.WhiteSpace.AtLeastOnce()
+                select new KeyValue {Key = flag, Value = value};
+
+            var scriptParser =
+                from parsedFlags in flagParser.Many()
+                from parsedScript in Parse.AnyChar.AtLeastOnce().Text()
+                select parsedScript;
+
+            var flags = flagParser.Many().TryParse(trimmed).Value;
+            var script = scriptParser.TryParse(trimmed).Value;
+
+            return new ScriptToRun()
+            {
+                Args = flags.ToList(),
+                Script = script,
+            };
+        }
+
+
         private async Task<Message> LinqpadAsync(IncomingMessage message, string value)
         {
-            var fileName =
-                await WriteScriptToFile(value.StartsWith("s") ? value.Substring(1) : value, value.StartsWith("s"));
+            var script = ParseInput(value);
 
-            Log.Debug("Linqpad handling {Command} with input {Input} to filename {FileName}", message, value, fileName);
+            var fileName = await WriteScriptToFile(script.Script);
+
+            Log.Debug("Linqpad handling {Command} processed as args: {Args}, script {Script} to filename {FileName}", message, script.Args, script.Script, fileName);
 
             var compilation = await LINQPad.Util.CompileAsync(fileName);
 
@@ -69,7 +116,7 @@ namespace TeamBot.Features.LinqPad
                 return replyMessage;
             }
 
-            var executer = compilation.Run(LINQPad.QueryResultFormat.Text);
+            var executer = compilation.Run(LINQPad.QueryResultFormat.Text, script.Args);
 
             var result = await executer.AsStringAsync();
 
@@ -81,11 +128,9 @@ namespace TeamBot.Features.LinqPad
             };
         }
 
-        private async Task<string> WriteScriptToFile(string script, bool isStatement)
+        private async Task<string> WriteScriptToFile(string script)
         {
             var fileName = Path.GetTempFileName();
-
-            script = $"{(isStatement ? "<Query Kind=\"Statement\" />" + Environment.NewLine : "")} {script}";
 
             var fileContents = Encoding.UTF8.GetBytes(script);
             using (var fileStream = File.OpenWrite(fileName))
